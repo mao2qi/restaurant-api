852bb6595471b85e7126c081e6a1c7223b299009
mao2qi <117254057+mao2qi@users.noreply.github.com>
2020-12-03 12:35:23 +0000

    raise version number

--- app.vue
+++ app.vue
@@ -24,4 +24,7 @@ useHead({
     meta: [
         {
             name: 'viewport',
-            content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-s
+            content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
+        },
+    ],
+}
