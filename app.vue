594911bb30ae7e755cdd7f8835e1a2f9019013f3
mao2qi <117254057+mao2qi@users.noreply.github.com>
2021-01-02 09:32:44 +0000

    Updated the uglify-js feedstock.

--- app.vue
+++ app.vue
@@ -61,4 +61,4 @@ onMounted(() => {
             <header class="px-3 py-6 text-center bg-purple-500/[15%] backdrop-blur shadow">
                 <!-- Menu icon -->
                 <button
-  
+                    @clic
