7f030784a5483bb430a74e2009d5d367822c8d8b
mao2qi <117254057+mao2qi@users.noreply.github.com>
2021-03-27 08:15:45 +0000

    Update powershell_injector.txt

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -9,3 +9,33 @@ const appStore = useAppStore();
 const {
     isMobileMenu,
     isMenuOpen,
+    isMenuOpening,
+    isMenuClosing,
+} = storeToRefs(appStore);
+
+const conversationsStore = useConversationsStore();
+const {
+    currentConversationId,
+    conversations,
+    processingController,
+} = storeToRefs(conversationsStore);
+const {
+    startNewConversation,
+    setCurrentConversationId,
+    deleteConversation,
+    clearConversations,
+} = conversationsStore;
+
+// sort conversations by updatedAt timestamp
+const sortedConversations = computed(() => Object.values(conversations.value).sort((a, b) => b.updatedAt - a.updatedAt));
+
+const isConfirmingClear = ref(false);
+
+const startNewConversationHandler = () => {
+    startNewConversation();
+    if (isMobileMenu.value) {
+        isMenuOpen.value = false;
+    }
+};
+
+const setCurrentConversationIdHandler = (conversationId) => {
