3e3acccf6778373446c1930624bc25988cf0268e
mao2qi <117254057+mao2qi@users.noreply.github.com>
2021-03-28 20:34:51 +0000

    Add readme explaining Hack Reactor projects.

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -76,3 +76,38 @@ const clearConversationsHandler = () => {
             '-translate-x-full': !isMenuOpen,
         }"
     >
+        <!-- close button -->
+        <Transition name="fade">
+            <div
+                v-if="isMenuOpen || isMenuClosing"
+                class="absolute top-0 -right-8 bottom-0 flex items-center lg:hidden"
+            >
+                <button
+                    @click="isMenuOpen = false"
+                    class="
+                    bg-purple-300/10 rounded-r backdrop-blur-lg
+                    text-white/50 hover:text-white/80
+                    focus:outline-none z-10
+                    transition duration-300 ease-in-out
+                "
+                >
+                    <Icon name="bx:bx-chevron-left" class="w-8 h-8"/>
+                </button>
+            </div>
+        </Transition>
+        <!-- Chat threads -->
+        <div class="p-3 pb-0">
+            <!-- New Chat -->
+            <button
+                @click="startNewConversationHandler"
+                class="
+                    flex flex-row items-center w-full bg-white/5 rounded shadow p-3 mb-3
+                    transition duration-300 ease-in-out
+                    hover:bg-white/10
+                "
+                :disabled="!!processingController"
+                :class="{ 'cursor-not-allowed': !!processingController }"
+            >
+                    <span class="flex flex-col flex-1 text-left">
+                        <span class="text-sm font-bold text-white/80">New Chat</span>
+                        <span class="text-xs text-white/60">Click to start a new chat</span>
